ca5254fb676278888382a3bf66285d6cd3379440
Daniel Llewellyn <daniel@bowlhat.net>
2019-01-29 15:37:20 +0000

    Add filtering of test and hello-world snaps
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ Promise.all(config.spider.snaps.stores.map(async (store) => {
     let api = new SnapApi(store.url, store.domain);
     const snaps = await api.list();
 
-    const bases = snaps.reduce((carry, snap) => {
+    const non_hello_or_test_snaps = snaps.reduce((carry, snap) => {
+        const name = ('name' in snap) ? snap.name : '';
+        if (name !== '' && !name.match(/^(hello|test)-/) && !name.match(/-test$/)) {
+            carry = [...carry, snap];
+        }
+        return carry;
+    }, []);
+
+    const bases = non_hello_or_test_snaps.reduce((carry, snap) => {
         if ('base' in snap && snap.base !== '') {
             carry = [...carry, snap.base];
         }
@@ -17,7 +25,8 @@ Promise.all(config.spider.snaps.stores.map(async (store) => {
     const found_base_names = [...new Set(bases)];
 
     console.log(`Found ${snaps.length} snaps of which ${bases.length} specified a base snap.`);
-    console.log(`Base snap names found: ${found_base_names.join(', ')}.`);
+    console.log(`There are ${snaps.length - non_hello_or_test_snaps.length} snaps that match '*-test', 'test-*', and 'hello-*' leaving ${non_hello_or_test_snaps.length} in the set.`)
+    console.log(`Base snap names found in non-hello and non-test snaps: ${found_base_names.join(', ')}.`);
     for (const name of found_base_names) {
         const this_base = bases.reduce((carry, base) => {
             if (base === name) {
@@ -25,6 +34,6 @@ Promise.all(config.spider.snaps.stores.map(async (store) => {
             }
             return carry;
         }, []);
-        console.log(`${this_base.length} snaps specified ${name} as their base snap.`);
+        console.log(`${this_base.length} non-hello and non-test snaps specified ${name} as their base snap.`);
     }
 }));
